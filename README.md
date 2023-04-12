bea737715a710fc465145dd8299e4c50a9bcd004
andreasgriffin <andreasgriffin@proton.me>
2023-04-12 22:23:49 +0200

    working

--- README.md
+++ README.md
@@ -11,13 +11,14 @@ You'll need `wasm-pack` and `wasm-bindgen` installed.
 To build the project:
 
 ```
-wasm-pack build --target bundler --out-dir dist
+wasm-pack build --release --target web
 ```
 
 To run the example:
 
 ```
-npx http-server --cors -c-1 -p 8080 --mime-types '{"wasm": "application/wasm"}'
+npx http-server --cors -c-1 -p 8080 --mime-types '{"wasm": "application/wasm", "html": "text/html", "css": "text/css", "js": "text/javascript", "json": "application/json", "jpg": "image/jpeg", "png": "image/png", "gif": "image/gif", "mp3": "audio/mpeg", "ogg": "audio/ogg", "wav": "audio/wav", "mp4": "video/mp4", "webm": "video/webm"}'
+
 ```
 
 
--- bdk.js
+++ bdk.js
@@ -1,21 +1,27 @@
-import * as bdk from './dist/js_example.js';
-await bdk.init('undefined');
+import init from './pkg/js_example.js';  // see https://stackoverflow.com/questions/64308461/failed-to-load-wasm-application
+import * as bdk  from '../pkg/js_example.js';
 
+function run () {
 
-// Re-enable this line if you want the mnemonic to be generated
-// let mnemonic = bdk.generate_mnemonic()
-let mnemonic = "vapor winter snake hand reason airport text develop enrich abandon oyster filter";
-console.log(mnemonic)
+    // Re-enable this line if you want the mnemonic to be generated
+    // let mnemonic = bdk.generate_mnemonic()
+    let mnemonic = "vapor winter snake hand reason airport text develop enrich abandon oyster filter";
+    console.log(mnemonic)
 
-let network = new bdk.Network("testnet")
-let desc = bdk.mnemonic_to_bip86_tap_descriptor(mnemonic, null, network)
-let wallet = new bdk.Wallet(desc, network)
+    let network = new bdk.Network("testnet")
+    let desc = bdk.mnemonic_to_bip86_tap_descriptor(mnemonic, null, network)
+    let wallet = new bdk.Wallet(desc, network)
 
-// Printing an address
-console.log(wallet.get_new_address())
+    // Printing an address
+    console.log(wallet.get_new_address())
 
-// The balance is zero before syncing
-console.log(wallet.get_total_balance())
+    // The balance is zero before syncing
+    console.log(wallet.get_total_balance())
 
-// But it might change after you sync
-wallet.sync().then(() => console.log(wallet.get_total_balance()))
+    // But it might change after you sync
+    wallet.sync().then(() => console.log(wallet.get_total_balance()))
+
+}
+
+init().then(run)
+
