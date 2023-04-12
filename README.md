db8b09ba0a4b2e0b3d4a0a2a717fd0eb28c5dc49
andreasgriffin <andreasgriffin@proton.me>
2023-04-12 22:05:25 +0200

    testing

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# safe-bitcoin
+# Running BDK in a webbrowser
+
+
+
+
+
+## Setup
+
+You'll need `wasm-pack` and `wasm-bindgen` installed.
+
+To build the project:
+
+```
+wasm-pack build --target bundler --out-dir dist
+```
+
+To run the example:
+
+```
+npx http-server --cors -c-1 -p 8080 --mime-types '{"wasm": "application/wasm"}'
+```
+
+
+
---
+++ bdk.js
@@ -0,0 +1,21 @@
+import * as bdk from './dist/js_example.js';
+await bdk.init('undefined');
+
+
+// Re-enable this line if you want the mnemonic to be generated
+// let mnemonic = bdk.generate_mnemonic()
+let mnemonic = "vapor winter snake hand reason airport text develop enrich abandon oyster filter";
+console.log(mnemonic)
+
+let network = new bdk.Network("testnet")
+let desc = bdk.mnemonic_to_bip86_tap_descriptor(mnemonic, null, network)
+let wallet = new bdk.Wallet(desc, network)
+
+// Printing an address
+console.log(wallet.get_new_address())
+
+// The balance is zero before syncing
+console.log(wallet.get_total_balance())
+
+// But it might change after you sync
+wallet.sync().then(() => console.log(wallet.get_total_balance()))
